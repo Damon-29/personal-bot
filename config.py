e6e9f90360e2506f89792f1dcff6fdd7fbf6d949
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-28 02:06:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,25 +2,25 @@ import os
 
 TEST_WEBHOOK = os.getenv("TEST_WEBHOOK")
 
-REDDIT_SOURCES = [
-    {
+REDDIT_SOURCES = {
+    "wuwa": {
         "game": "Wuthering Waves",
         "name": "WutheringWavesLeaks",
         "rss": "https://www.reddit.com/r/WutheringWavesLeaks/new/.rss",
     },
-    {
+    "genshin": {
         "game": "Genshin Impact",
         "name": "Genshin_Impact_Leaks",
         "rss": "https://www.reddit.com/r/Genshin_Impact_Leaks/new/.rss",
     },
-    {
+    "zzz": {
         "game": "Zenless Zone Zero",
         "name": "Zenlesszonezeroleaks_",
         "rss": "https://www.reddit.com/r/Zenlesszonezeroleaks_/new/.rss",
     },
-    {
+    "hsr": {
         "game": "Honkai: Star Rail",
         "name": "HonkaiStarRail_leaks",
         "rss": "https://www.reddit.com/r/HonkaiStarRail_leaks/new/.rss",
     },
-]
+}
