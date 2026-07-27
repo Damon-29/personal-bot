c8ea06942bffadc49eac8368501847eba0c7a5ff
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-28 00:50:34 +0530

    Update post.py

--- models/post.py
+++ models/post.py
@@ -1,1 +1,16 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class Post:
+    id: str
+    game: str
+    source: str
+
+    title: str
+    url: str
+
+    published: str
+
+    author: str = ""
+    thumbnail: str = ""
