bb6a99421d10c679a30f0d987698e8d2a2952a8c
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-28 01:34:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,67 +1,21 @@
-import os
-
-TEST_WEBHOOK = os.getenv("TEST_WEBHOOK")
-
-CHECK_INTERVAL = 300
-
 REDDIT_SOURCES = [
-
-    # =======================
-    # Wuthering Waves
-    # =======================
-
     {
-        "game": "wuwa",
-        "name": "WutheringWaves",
-        "rss": "https://www.reddit.com/r/WutheringWaves/new/.rss",
-    },
-    {
-        "game": "wuwa",
+        "game": "Wuthering Waves",
         "name": "WutheringWavesLeaks",
         "rss": "https://www.reddit.com/r/WutheringWavesLeaks/new/.rss",
     },
-
-    # =======================
-    # Genshin
-    # =======================
-
     {
-        "game": "genshin",
-        "name": "Genshin_Impact",
-        "rss": "https://www.reddit.com/r/Genshin_Impact/new/.rss",
-    },
-    {
-        "game": "genshin",
+        "game": "Genshin Impact",
         "name": "Genshin_Impact_Leaks",
         "rss": "https://www.reddit.com/r/Genshin_Impact_Leaks/new/.rss",
     },
-
-    # =======================
-    # ZZZ
-    # =======================
-
     {
-        "game": "zzz",
-        "name": "ZZZ_Official",
-        "rss": "https://www.reddit.com/r/ZZZ_Official/new/.rss",
-    },
-    {
-        "game": "zzz",
+        "game": "Zenless Zone Zero",
         "name": "Zenlesszonezeroleaks_",
         "rss": "https://www.reddit.com/r/Zenlesszonezeroleaks_/new/.rss",
     },
-
-    # =======================
-    # HSR
-    # =======================
-
     {
-        "game": "hsr",
-        "name": "HonkaiStarRail",
-        "rss": "https://www.reddit.com/r/HonkaiStarRail/new/.rss",
-    },
-    {
-        "game": "hsr",
+        "game": "Honkai Star Rail",
         "name": "HonkaiStarRail_leaks",
         "rss": "https://www.reddit.com/r/HonkaiStarRail_leaks/new/.rss",
     },
