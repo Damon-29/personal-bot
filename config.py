fc9b369960b10b91080a4bd5a23938004e39d96d
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-28 01:02:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,77 @@ import os
 
 TEST_WEBHOOK = os.getenv("TEST_WEBHOOK")
 
+CHECK_INTERVAL = 300
+
+
 REDDIT_SOURCES = [
+
+    # =======================
+    # Wuthering Waves
+    # =======================
+
     {
         "game": "wuwa",
         "name": "WutheringWaves",
-        "url": "https://www.reddit.com/r/WutheringWaves/.json",
-        "allowed_flairs": [
-            "Official News"
-        ]
+        "url": "https://www.reddit.com/r/WutheringWaves/new.json?limit=25",
+        "allowed_flairs": ["Official News"]
     },
     {
         "game": "wuwa",
         "name": "WutheringWavesLeaks",
-        "url": "https://www.reddit.com/r/WutheringWavesLeaks/.json",
+        "url": "https://www.reddit.com/r/WutheringWavesLeaks/new.json?limit=25",
         "allowed_flairs": []
     },
+
+    # =======================
+    # Genshin Impact
+    # =======================
+
+    {
+        "game": "genshin",
+        "name": "Genshin_Impact",
+        "url": "https://www.reddit.com/r/Genshin_Impact/new.json?limit=25",
+        "allowed_flairs": ["Official Post"]
+    },
+    {
+        "game": "genshin",
+        "name": "Genshin_Impact_Leaks",
+        "url": "https://www.reddit.com/r/Genshin_Impact_Leaks/new.json?limit=25",
+        "allowed_flairs": []
+    },
+
+    # =======================
+    # Zenless Zone Zero
+    # =======================
+
+    {
+        "game": "zzz",
+        "name": "ZZZ_Official",
+        "url": "https://www.reddit.com/r/ZZZ_Official/new.json?limit=25",
+        "allowed_flairs": ["Official Media"]
+    },
+    {
+        "game": "zzz",
+        "name": "Zenlesszonezeroleaks_",
+        "url": "https://www.reddit.com/r/Zenlesszonezeroleaks_/new.json?limit=25",
+        "allowed_flairs": []
+    },
+
+    # =======================
+    # Honkai Star Rail
+    # =======================
+
+    {
+        "game": "hsr",
+        "name": "HonkaiStarRail",
+        "url": "https://www.reddit.com/r/HonkaiStarRail/new.json?limit=25",
+        "allowed_flairs": ["Official Announcement"]
+    },
+    {
+        "game": "hsr",
+        "name": "HonkaiStarRail_leaks",
+        "url": "https://www.reddit.com/r/HonkaiStarRail_leaks/new.json?limit=25",
+        "allowed_flairs": []
+    }
+
 ]
