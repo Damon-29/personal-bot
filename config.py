372cd76966d75d54760d29d26709a30330551624
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-28 01:44:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,7 @@
 REDDIT_SOURCES = [
     {
-        "game": "Wuthering Waves",
-        "name": "WutheringWavesLeaks",
-        "rss": "https://www.reddit.com/r/WutheringWavesLeaks/new/.rss",
-    },
-    {
         "game": "Genshin Impact",
         "name": "Genshin_Impact_Leaks",
         "rss": "https://www.reddit.com/r/Genshin_Impact_Leaks/new/.rss",
-    },
-    {
-        "game": "Zenless Zone Zero",
-        "name": "ZenlessZoneZeroLeaks",
-        "rss": "https://www.reddit.com/r/ZenlessZoneZeroLeaks_/new/.rss",
-    },
-    {
-        "game": "Honkai: Star Rail",
-        "name": "HonkaiStarRail_leaks",
-        "rss": "https://www.reddit.com/r/HonkaiStarRail_leaks/new/.rss",
-    },
+    }
 ]
