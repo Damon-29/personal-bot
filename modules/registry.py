4a59b051a39e379d0b9e83451942c7aa4e826458
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-28 01:06:07 +0530

    Update registry.py

--- modules/registry.py
+++ modules/registry.py
@@ -1,1 +1,12 @@
 
+from modules import reddit
+# from modules import youtube
+# from modules import twitter
+
+
+def load_modules():
+    return [
+        reddit,
+        # youtube,
+        # twitter
+    ]
