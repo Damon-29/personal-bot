82b8c5fc5c8ba00ebea23f9e6eff58d359501330
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-28 00:53:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from models.post import Post
+from services.discord import send_post
 
+
+post = Post(
+    id="1",
+    game="wuwa",
+    source="test",
+    title="Hello Discord!",
+    url="https://google.com",
+    published="today"
+)
+
+send_post(post)
