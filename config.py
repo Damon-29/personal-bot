590e07ffe6dbf47c3daa9c36ed7efeec4b4029f4
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-28 13:29:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,11 @@ REDDIT_SOURCES = {
         "name": "WutheringWavesLeaks",
         "rss": "https://www.reddit.com/r/WutheringWavesLeaks/new/.rss",
     },
+    "WUWA_OFFICIAL": {
+        "game": "wuwa",
+        "name": "WuWa Official",
+        "rss": "https://www.reddit.com/r/WutheringWaves/.rss",
+    },
     "genshin": {
         "game": "Genshin Impact",
         "name": "Genshin_Impact_Leaks",
